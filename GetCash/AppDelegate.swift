8ec6f90b091ba1669cf2c26d5e75f60cfd634a64
hekang1992 <hekang19921111@hotmail.com>
2025-12-19 14:02:06 +0800

    init-app

---
+++ GetCash/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  GetCash
+//
+//  Created by hekang on 2025/12/19.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+    
+    
+}
+
