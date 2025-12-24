c5046b258e8e8b578c03103cf25f02585acfb393
hekang1992 <hekang19921111@hotmail.com>
2025-12-24 10:56:10 +0800

    location-manager

--- GetCash/Login/AppLocationManager.swift
+++ GetCash/Login/AppLocationManager.swift
@@ -10,45 +10,45 @@ import Foundation
 import CoreLocation
 
 class AppLocationManager: NSObject {
-
+    
     static let shared = AppLocationManager()
-
+    
     private let locationManager = CLLocationManager()
     private let geocoder = CLGeocoder()
-
+    
     private var completion: (([String: String]?) -> Void)?
     private var debounceWorkItem: DispatchWorkItem?
-
+    
     private let debounceInterval: TimeInterval = 2.0
-
+    
     override init() {
         super.init()
         locationManager.delegate = self
     }
-
+    
     func getCurrentLocation(completion: @escaping ([String: String]?) -> Void) {
         self.completion = completion
-
+        
         switch locationManager.authorizationStatus {
         case .notDetermined:
             locationManager.requestWhenInUseAuthorization()
-
+            
         case .authorizedWhenInUse, .authorizedAlways:
             startLocationUpdate()
-
+            
         case .denied, .restricted:
             finish(nil)
-
+            
         @unknown default:
             finish(nil)
         }
     }
-
+    
     private func startLocationUpdate() {
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.startUpdatingLocation()
     }
-
+    
     private func finish(_ result: [String: String]?) {
         DispatchQueue.main.async {
             self.completion?(result)
@@ -58,57 +58,57 @@ class AppLocationManager: NSObject {
 
 // MARK: - CLLocationManagerDelegate
 extension AppLocationManager: CLLocationManagerDelegate {
-
+    
     func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
         switch manager.authorizationStatus {
         case .authorizedWhenInUse, .authorizedAlways:
             startLocationUpdate()
-
+            
         case .denied, .restricted:
             finish(nil)
-
+            
         case .notDetermined:
             break
-
+            
         @unknown default:
             finish(nil)
         }
     }
-
+    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         guard let location = locations.last else { return }
-
+        
         locationManager.stopUpdatingLocation()
         debounceWorkItem?.cancel()
-
+        
         let workItem = DispatchWorkItem { [weak self] in
             guard let self else { return }
-
+            
             self.geocoder.reverseGeocodeLocation(location) { placemarks, _ in
                 let placemark = placemarks?.first
-
+                
                 let locationJson: [String: String] = [
-                    "local": placemark?.administrativeArea ?? "",
-                    "enough": placemark?.isoCountryCode ?? "",
-                    "opting": placemark?.country ?? "",
-                    "mutation": placemark?.thoroughfare ?? "",
                     "basic": placemark?.locality ?? "",
                     "evolutionary": placemark?.subLocality ?? "",
                     "perturb": String(format: "%.6f", location.coordinate.latitude),
-                    "compute": String(format: "%.6f", location.coordinate.longitude)
+                    "compute": String(format: "%.6f", location.coordinate.longitude),
+                    "local": placemark?.administrativeArea ?? "",
+                    "enough": placemark?.isoCountryCode ?? "",
+                    "opting": placemark?.country ?? "",
+                    "mutation": placemark?.thoroughfare ?? ""
                 ]
-
+                
                 self.finish(locationJson)
             }
         }
-
+        
         debounceWorkItem = workItem
         DispatchQueue.main.asyncAfter(
             deadline: .now() + debounceInterval,
             execute: workItem
         )
     }
-
+    
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         locationManager.stopUpdatingLocation()
         finish(nil)
